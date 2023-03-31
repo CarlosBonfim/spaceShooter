a900e340a5c71ad9339c271635979a0dd5520722
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-03-31 10:25:01 -0400

    func gameover, start e correcoes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const yourShip = document.querySelector(".player-shooter");
 const playArea = document.querySelector("#main-play-area");
 const aliensImg = ['img/monster-1.png', 'img/monster-2.png','img/monster-3.png']
-
+const instructionsText = document.querySelector('.game-instructions')
+const startButton = document.querySelector('.start-button')
+let alienInterval;
 //movimentacao
 function flyShip(event) {
     if (event.key === "ArrowUp") {
@@ -120,6 +122,34 @@ function checkLaserCollision(laser, alien) {
         return false;
     }
 }
+startButton.addEventListener("click", (event) => {
+    playGame();
+});
+//start
+function playGame() {
+    startButton.style.display = "none";
+    instructionsText.style.display = "none";
+    window.addEventListener("keydown", flyShip);
+    alienInterval = setInterval(() => {
+        createAliens();
+    }, 2000);
+}
 
-window.addEventListener("keydown", flyShip);
-createAliens();
+//game over
+function gameOver(){
+    window.removeEventListener("keydown", flyShip);
+    clearInterval(alienInterval);
+    let aliens = document.querySelectorAll(".alien");
+    aliens.forEach((alien) => alien.remove());
+    let lasers = document.querySelectorAll(".laser");
+    lasers.forEach((laser) => laser.remove());
+    setTimeout(() => {
+        alert("game over");
+        yourShip.style.top = "250px";
+        startButton.style.display = "block";
+        instructionsText.style.display = "block";
+    });
+}
+
+// window.addEventListener("keydown", flyShip);
+// createAliens();
